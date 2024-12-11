2be5db9c150e0106e96889e62328834b07f7ea95
Jiekai Sun - jiekai.sun@studio.unibo.it <jiekai.sun@studio.unibo.it>
2024-12-11 11:15:36 +0100

    Configured backend java project

---
+++ control-unit-backend/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    // Apply the java plugin to add support for Java
+    java
+
+    // Apply the application plugin to add support for building a CLI application
+    // You can run your app via task "run": ./gradlew run
+    application
+
+    /*
+     * Adds tasks to export a runnable jar.
+     * In order to create it, launch the "shadowJar" task.
+     * The runnable jar will be found in build/libs/projectname-all.jar
+     */
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+repositories { // Where to search for dependencies
+    mavenCentral()
+}
+
+dependencies {
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("it.unibo.backend.App")
+}
