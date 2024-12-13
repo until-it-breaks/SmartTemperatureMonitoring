f1364bca9ae3b8dffbe6f715772b6b8856fd074b
Jiekai Sun <jiekai.sun@studio.unibo.it>
2024-12-13 15:11:10 +0100

    Implemented a MQTT client capable of publishing messages

--- control-unit-backend/src/main/java/it/unibo/backend/MQTTAgent.java
+++ control-unit-backend/src/main/java/it/unibo/backend/MQTTAgent.java
@@ -2,36 +2,73 @@ package it.unibo.backend;
 
 import io.netty.handler.codec.mqtt.MqttQoS;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonObject;
 import io.vertx.mqtt.MqttClient;
+import io.vertx.mqtt.MqttClientOptions;
 
 public class MQTTAgent extends AbstractVerticle {
     private static final String BROKER_ADDRESS = "34.154.239.184";
+    private static final int BROKER_PORT = 1883;
     private static final String TOPIC_NAME = "esiot-2024";
-
-    public MQTTAgent() {
-    }
+    private MqttClient client;
 
     @Override
     public void start() {
-        MqttClient client = MqttClient.create(vertx);
-        client.connect(1883, BROKER_ADDRESS, c-> {
-            log("connected");
+        MqttClientOptions options = new MqttClientOptions();
+        options.setClientId("ControlUnitClient");
+        options.setKeepAliveInterval(60);
 
-            log("subscribing...");
-            client.publishHandler(s -> {
-                System.out.println("There are new message in topic: " + s.topicName());
-                System.out.println("Content of the message: " + s.payload().toString());
-                System.out.println("QoS: " + s.qosLevel());
-            })
-            .subscribe(TOPIC_NAME, 2);
-
-            log("publishing a msg");
-            client.publish(TOPIC_NAME, Buffer.buffer("hello"), MqttQoS.AT_LEAST_ONCE, false, false);
+        client = MqttClient.create(vertx, options);
+        client.connect(BROKER_PORT, BROKER_ADDRESS,  result -> {
+            if (result.succeeded()) {
+                log("Connected to MQTT broker at: " + BROKER_ADDRESS);
+                log("subscribing...");
+                this.subscribeToTopic();
+            } else {
+                log("Failed to connect to MQTT broker");
+            }
         });
     }
 
+    public void subscribeToTopic() {
+        client.subscribe(TOPIC_NAME, MqttQoS.AT_LEAST_ONCE.value(), result -> {
+            if (result.succeeded()) {
+                log("Subscribed to topic: " + TOPIC_NAME);
+            } else {
+                log("Failed to subscribe to topic: " + result.cause());
+            }
+        });
+
+        client.publishHandler(message -> {
+            log("Received message: " + message.payload().toString());
+        });
+    }
+
+    public void publishMessage(String messageContent) {
+        JsonObject jsonMessage = new JsonObject();
+        jsonMessage.put("message", messageContent);
+        log("Publishing a message");
+        client.publish(TOPIC_NAME, Buffer.buffer(jsonMessage.encode()), MqttQoS.AT_LEAST_ONCE, false, false);
+    }
+
     private void log(String msg) {
-		System.out.println("[MQTT AGENT] "+msg);
+		System.out.println("[MQTT AGENT] " + msg);
 	}
+
+    public static void main(String[] args) throws InterruptedException {
+        Vertx vertx = Vertx.vertx();
+        MQTTAgent agent = new MQTTAgent();
+        vertx.deployVerticle(agent, result -> {
+            if (result.succeeded()) {
+                System.out.println("MQTTAgent deployed successfully");
+            } else {
+                System.out.println("MQTTAgent deployment failed: " + result.cause());
+            }
+        });
+        // Wait for agent to fully deploy.
+        Thread.sleep(2000);
+        agent.publishMessage("ciaone");
+    }
 }
