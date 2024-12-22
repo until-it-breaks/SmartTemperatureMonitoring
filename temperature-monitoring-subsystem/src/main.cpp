7a7a656b4a96f4c69be1b87c4dff04e84fbc0787
Weijie Fu <weijie.fu@studio.unibo.it>
2024-12-22 18:14:25 +0100

    testing temperature controller in a task

--- temperature-monitoring-subsystem/src/main.cpp
+++ temperature-monitoring-subsystem/src/main.cpp
@@ -5,14 +5,35 @@
 
 LedController* ledController;
 TemperatureController* tempController;
+TaskHandle_t Task1;
 
+void Task1code(void* parameter);
 
 void setup() {
+  Serial.begin(115200); 
   ledController = new LedController(new Led(GREEN_LED_PIN), new Led(RED_LED_PIN));
   tempController = new TemperatureController(new TemperatureSensor(TEMP_PIN));
+  ledController->switchOnGreen();
+  ledController->switchOnRed();
+
+  xTaskCreatePinnedToCore(Task1code, "Task1", 10000, NULL, 1, &Task1, 0);       
 }
 
+
+void Task1code(void* parameter){
+  Serial.print("Task1 is running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for(;;){
+    tempController->readTemp();
+    delay(500);
+    Serial.println("current temperature: " + String(tempController->getTemp()));
+    delay(500);
+  } 
+}
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(1000);
 }
 
