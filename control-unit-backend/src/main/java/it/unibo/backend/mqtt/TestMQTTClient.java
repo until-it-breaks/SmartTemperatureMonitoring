9e97e36ccc869691afb8d98c7dd3404cc2308d06
Jiekai Sun <jiekai.sun@studio.unibo.it>
2024-12-25 22:26:34 +0100

    Moved exception throw level to main method

--- control-unit-backend/src/main/java/it/unibo/backend/mqtt/TestMQTTClient.java
+++ control-unit-backend/src/main/java/it/unibo/backend/mqtt/TestMQTTClient.java
@@ -7,14 +7,10 @@ import io.vertx.core.json.JsonObject;
  */
 public class TestMQTTClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         MQTTClient client = new MQTTClient("34.154.239.184", 1883);
         client.start();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Thread.sleep(2000);
         JsonObject data = new JsonObject();
         data.put("welcomeMessage", "ciao");
         client.publish("esiot", data);
