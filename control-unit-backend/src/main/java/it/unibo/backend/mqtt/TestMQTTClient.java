eb76dfdd142b5d2edfeb4cc26d6f574ead3d1993
Jiekai Sun <jiekai.sun@studio.unibo.it>
2024-12-25 22:25:46 +0100

    Removed unused logger

--- control-unit-backend/src/main/java/it/unibo/backend/mqtt/TestMQTTClient.java
+++ control-unit-backend/src/main/java/it/unibo/backend/mqtt/TestMQTTClient.java
@@ -1,15 +1,11 @@
 package it.unibo.backend.mqtt;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import io.vertx.core.json.JsonObject;
 
 /**
  * Unrelated to the backend. Just a MQTT test class.
  */
 public class TestMQTTClient {
-    public static final Logger logger = LoggerFactory.getLogger(TestMQTTClient.class);
 
     public static void main(String[] args) {
         MQTTClient client = new MQTTClient("34.154.239.184", 1883);
